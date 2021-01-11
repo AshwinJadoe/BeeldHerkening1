73eefa00fb023ae80591c7d3915c3c10233ead8f
Ashwinjadoe <ashwinjadoe@gmail.com>
2021-01-11 16:16:19 +0100

    Add project files.

---
+++ BeeldHerkening1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+using BeeldHerkening1ML.Model;
+namespace BeeldHerkening1
+
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Debug.WriteLine(" Object is not valid for this category.");
+      // Add input data
+      var input = new ModelInput();
+      input.ImageSource = "C:\\Users\\Ashwi\\Downloads\\vos.jpg";
+      
+      // Load model and predict output of sample data
+      ModelOutput result = ConsumeModel.Predict(input);
+      Debug.WriteLine(result.Prediction);
+      Console.WriteLine(result.Prediction);
+    }
+  }
+}
+
